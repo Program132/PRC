b5f59868067fb53f4b8293d5402c329d64b77345
beyptitloup <67320703+Program132@users.noreply.github.com>
2021-04-10 08:37:21 +0200

    Fix class

--- include/printing.hpp
+++ include/printing.hpp
@@ -5,14 +5,31 @@
 #include <sstream>
 #include <string>
 #include <iterator>
+#include <string_view>
 
 namespace N
 {    
     class Printing
     {
+        private:
+            void missingA()
+            {
+                std::cout << "Error Print : You need start and finish your print with a other \" if you want send a text  !" << std::endl;
+            }
+            void missingOther(int const& arg)
+            {
+                if (arg == 1)
+                {
+                    std::cout << "Error Print : You need finish your print with a other \" if you want send a text !" << std::endl;
+                }
+                else if (arg == 2)
+                {
+                    std::cout << "Error Print : You need start your print with a other \" if you want send a text !" << std::endl;
+                }
+            }
+
         public:
-            template <typename pr>
-            void doprint(pr const& arg)
+            void do_print(std::string const& arg)
             {
                 std::istringstream iss(arg);
                 std::vector<char> checkGuillemets((std::istream_iterator<char>(iss)), std::istream_iterator<char>());;    
@@ -27,15 +44,15 @@ namespace N
                 }
                 else if (checkGuillemets.back() != '"') 
                 {
-                    std::cout << "Error Print : You need finish your print with a other \" if you want send a text !" << std::endl;
+                    missingOther(1);
                 }
                 else if (checkGuillemets[0] != '"' ) 
                 {
-                    std::cout << "Error Print : You need start your print with a other \" if you want send a text !" << std::endl;
+                    missingOther(2);
                 }
                 else if (checkGuillemets[0] != '"'  && checkGuillemets.back() != '"')
                 {
-                    std::cout << "Error Print : You need start and finish your print with a other \" if you want send a text  !" << std::endl;
+                    missingA();
                 }
             }
     };
